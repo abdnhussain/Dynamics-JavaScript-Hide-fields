6a5d481af6c8be94df52004262e6c46fb8194c7e
Abdullah Hussain <abdnhussain@gmail.com>
2020-08-18 15:33:53 +0100

    JS Code to hide fields

---
+++ JavaScript1.js
@@ -0,0 +1,14 @@
+﻿function hidegardensectionifpropertytypeisflat(executionContext) {
+    var formContext = executionContext.getFormContext();
+
+    var propertytype = formContext.getAttribute("ntt_propertytype").getText();
+
+    var generaltab = formContext.ui.tabs.get("propertygeneraltab");
+    var gardensection = generaltab.sections.get("propertygeneralgardeninfosection");
+
+    if (propertytype == "Flat") {
+        gardensection.setVisible(false);
+    } else {
+        gardensection.setVisible(true);
+    }
+}
